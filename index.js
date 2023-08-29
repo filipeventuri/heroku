a4d7788795015be3c414d9d87930161bfe8b48d0
Filipe Venturi <filipeventuri@icloud.com>
2023-08-28 21:32:06 -0300

    EJS

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
 app.get("/", (req,res)=>{
-    res.send("Teste funcionando!");
+    res.render("index");
 })
 
 app.listen(port, ()=>{
